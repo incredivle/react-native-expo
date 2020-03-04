e69daa189adc809345acf3092e647450fd685f1c
BrianGuerra2019 <Brian_BMX_10@live.com.mx>
2020-03-04 10:39:06 -0600

    Update

--- Medicos_Expo/App.js
+++ Medicos_Expo/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Navigation from "./app/navigation/Navigation";
-import { firebaseApp } from "./app/utils/Firebase";
-import { YellowBox } from "react-native";
+import {
+  firebaseApp
+} from "./app/utils/Firebase";
+import {
+  YellowBox
+} from "react-native";
 import _ from "lodash";
 
 YellowBox.ignoreWarnings(["componentWillReceiveProps"]);
@@ -9,9 +13,10 @@ const _console = _.clone(console);
 console.warn = message => {
   if (message.indexOf("componentWillReceiveProps") <= -1) {
     _console.warn(message);
+
   }
 };
 
 export default function App() {
-  return <Navigation />;
+  return <Navigation / > ;
 }
